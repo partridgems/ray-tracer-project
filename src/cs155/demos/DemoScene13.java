6fd9e328c64da64c06f81a0936e0b8019b7731f7
edenzik <edenzik@gmail.com>
2015-10-23 01:33:22 -0400

    last commit for the night

--- src/cs155/demos/DemoScene13.java
+++ src/cs155/demos/DemoScene13.java
@@ -16,9 +16,9 @@ import java.io.IOException;
 
 /**
  * This class draws cylinders and other objects.
- * In PA03 it is used to demonstrate the depth of field camera.
+ * In PA03 it is used to demonstrate the animation capability by shooting a basketball through a hoop..
  *
- * @author Michael
+ * @author Eden
  */
 public class DemoScene13 {
 
@@ -54,89 +54,79 @@ public class DemoScene13 {
         Material mat2 = new Material(new Color3D(0d, 0d, 0d), new Color3D(0d, 0d, 0d), new Color3D(1d, 1d, 0d), new Color3D(1d, 1d, 1d), 200);
         Material mat3 = new Material(new Color3D(0d, 0d, 0d), new Color3D(0d, 0d, 0d), new Color3D(0d, 1d, 1d), new Color3D(1d, 1d, 1d), 200);
         Material mat4 = new Material(new Color3D(0d, 0d, 0d), new Color3D(0d, 0d, 0d), new Color3D(1d, 0d, 1d), new Color3D(1d, 1d, 1d), 200);
-        Material mat5 = new Material(new Color3D(0d, 0d, 0d), new Color3D(0d, 0d, 0d), new Color3D(0d, 0d, 1d), new Color3D(1d, 1d, 0d), 200);
-        Material mat6 = new Material(new Color3D(0d, 0d, 0d), new Color3D(0d, 0d, 0d), new Color3D(0d, 0d, 1d), new Color3D(1d, 1d, 0d), 200);
+        Material mat5 = new Material(new Color3D(0d, 0d, 0d), new Color3D(0d, 0d, 0d), new Color3D(1d, 0d, 1d), new Color3D(1d, 1d, 1d), 200);
 
-        CheckerTexture tex = new CheckerTexture();
-        tex.scale(10, 10);
-        mat1.setTexture(tex);
-        mat1.setTexWeight(0.25);
-        CheckerTexture tex2 = new CheckerTexture();
-        tex2.scale(1000, 100);
-        mat2.setTexture(tex2);
-        mat2.setTexWeight(0.25);
-        CheckerTexture tex3 = new CheckerTexture();
-        tex3.scale(1000, 1000);
-        mat3.setTexture(tex3);
-        mat3.setTexWeight(0.25);
-        mat3.setReflect(0.0);
+        ImageTexture ballTexture = new ImageTexture("images/ballTexture.png");
+        ballTexture.scale(4000, 2666);
+        mat1.setTexture(ballTexture);
+        mat1.setTexWeight(0.95); //75;
 
-        ImageTexture tex4 = new ImageTexture("images/metal floor.jpg");
-        tex4.scale(50, 50);
-        mat4.setTexture(tex4);
-        mat4.setTexWeight(0.95); //75;
+        ImageTexture basketTexture = new ImageTexture("images/basketTexture.png");
+        basketTexture.scale(50, 50);
+        mat2.setTexture(basketTexture);
+        mat2.setTexWeight(0.95); //75;
 
-        ImageTexture tex6 = new ImageTexture("images/ea.png");
-        tex6.scale(50, 50);
-        mat6.setTexture(tex6);
-        mat6.setTexWeight(0.95); //75;
+        ImageTexture courtTexture = new ImageTexture("images/courtTexture.png");
+        courtTexture.scale(50, 50);
+        mat3.setTexture(courtTexture);
+        mat3.setTexWeight(0.95); //75;
 
-        ImageTexture tex5 = new ImageTexture("images/grass.png");
-        tex5.scale(50, 50);
-        mat5.setTexture(tex5);
-        mat5.setTexWeight(0.95);
 
-//		Cylinder3D cyl1 = new Cylinder3D(new Point3D(1,-1,-15),new Point3D(0,1,0),3.0,15.0,mat4);
-        Cylinder3D cyl2 = new Cylinder3D(new Point3D(25, -5, -15), new Point3D(0, 1, 0), 20.0, 25.0, mat4);
-        Sphere3D sp1 = new Sphere3D(new Point3D(10, 5, -200), 20.0, mat6);
-        Sphere3D sp2 = new Sphere3D(new Point3D(0, 5, -20), 20.0, mat4);
-        Plane3D pl1 = new Plane3D(new Point3D(0, -1, 0), new Point3D(0, 1, 0), mat5);
+        Sphere3D ball = new Sphere3D(new Point3D(10, 5, -30), 15.0, mat1);
+        Cylinder3D basket = new Cylinder3D(new Point3D(50, 30, -200), new Point3D(0, 1, 0), 30.0, 25.0, mat2);
+        Cylinder3D post = new Cylinder3D(new Point3D(50, 0, -215), new Point3D(0, 1, 0), 5 , 50, mat5);
+        Plane3D court = new Plane3D(new Point3D(0, -1, 0), new Point3D(0, 1, 0), mat3);
 
         // create the lights
         Light3D light1 = new Light3D(new Point3D(-12, 22, 18), .8);// this light comes from above on the left
         Light3D light2 = new Light3D(new Point3D(30, 15, 65), .6); // this light is from way above the scene
-//		Light3D light3 = new Light3D(new Point3D(0,2,-13),1.0); // this light is near the openings of both cylinders
-//		Light3D light4 = new Light3D(new Point3D(-2,13,-4),1.0); // this light is inside cylinder 1, toward the far end
 
-        cyl2.setInsideMat(mat1); // here we change the material on the inside of cylinder 2
+        basket.setInsideMat(mat5); // here we change the material on the inside of cylinder 2
 
-        GifCanvas3D mc = new GifCanvas3D("DemoScene13", 800, 800);
+        GifCanvas3D mc = new GifCanvas3D("Basketball", 800, 800);
         Transform3D camTransf = new Transform3D();
         // this transformation takes a few from above and to the right looking down at the cylinders
         // comment it out to see the view from the origin..
-        camTransf = camTransf.translate(0, 0, 0).rotateY(20).rotateX(45).translate(0, 0, 60);
-//		Camera3D cam = new Camera3D(mc,camTransf);
-//		Camera3D cam = new FisheyeCamera3D(mc, camTransf);
+        camTransf = camTransf.translate(0, 0, 0).rotateY(20).rotateX(45).translate(0, 50, 60);
         Camera3D cam = new DepthFieldCamera3D(mc, camTransf, 54, 10);
-//		scene.add(cyl1);
-        //scene.add(cyl2);
-        //	scene.add(cyl3);
-        scene.add(sp1);
-        //scene.add(sp2);
-        scene.add(pl1);
-        //scene.add(pl2);
-        scene.add(light1);
-        scene.add(light2);
-//		scene.add(light3);
-//		scene.add(light4);
+
+        scene.add(ball);
+        scene.add(basket);
+        scene.add(post);
+        scene.add(court);
         scene.add(cam);
 
         scene.setAnim(new Animator3D() {    //Implemented Animator3D methods, which allows for animations.
+	    int YDir = 1;
+            int ZDir = -1;
+	    double yDelta = 6 * YDir;
+            double zDelta = yDelta * .5;
+	    final double originalZ = ball.getCenter().getZ();
+	    final double originalX = ball.getCenter().getX();
+	    final double basketZ = basket.center.getZ();
+            final double distance = Math.abs(basketZ - originalZ);
             double count = 0;
-            int h = 2;
-            int k = 10;
+            double frames = distance/zDelta;
+	    final double basketX = basket.center.getX();
+	    final double deltaX = (basketX - originalX)/frames;
+	    double currentZ = originalZ;
+	    double correction = 30/frames;
 
             @Override
             public boolean hasNext() {
-                return count < 10;
+                return count < frames + 5;
             }
 
             @Override
             public Scene3D next() {
-                double x = count;
-                double y = x;
-                sp2.applyTrans(sp2.getTransform().translate(x, 0, 0));
-                count+=0.1;
+		cam.setTransform(Transform3D.compose(cam.getTransform(), new Transform3D().translate(0,.05*yDelta,0.01*yDelta)));
+		if (Math.abs(basketZ - currentZ) < Math.abs(originalZ - currentZ)){
+			YDir = -1;
+		}
+		
+                ball.applyTrans(new Transform3D().translate(deltaX, yDelta*YDir, (zDelta+correction)*-1));
+		currentZ -= zDelta;
+                count+=1;
                 return scene;
             }
         });
